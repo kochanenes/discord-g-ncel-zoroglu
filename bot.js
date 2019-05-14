5611e6d862a7b8976ecf60d42d5943a2d3699775
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-05-14 23:42:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -237,20 +237,7 @@ client.unload = command => {
       reject(e);
     }
   });
-};
-client.on('message', message => {
-    if (message.content === prefix + "sunucubilgi") {
-        const embed = new Discord.RichEmbed()
-            .addField ("Sunucu Adı", message.guild.name, true)
-            
-            .addField ("Sunucu ID", message.guild.id, true)
-            .addField ("Sunucu Sahibi", message.guild.owner)
-            .addField ("Toplam Üye Sayısı", message.guild.memberCount)
-            .setFooter("Olusturma Tarihi " + message.guild.createdAt)
-            .setColor(0x000001)
-        return message.channel.sendEmbed(embed)
-    }   
-});
+
 
 
    
