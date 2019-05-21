6a8a2579221c45e3e5f658a2ae6d52a1557ee4cf
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-05-21 20:50:01 +0300

    Update espiri.js

--- komutlar/espiri.js
+++ komutlar/espiri.js
@@ -2,10 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message) => {
-   message.channel.send('Espirimi İstiyorsun Hemen Geliyor Bir Tane...').then(message => {
-   var espriler = ['**Ayda 500 dolar kazmak istermis o zaman aya çık... Espiri Sahibi = ** __**Erkan - ByBaGo / 15**__', '**Rock yapmayan kişiye ne denir? - Yaprock. ** ' , '**Sinemada on dakika ara dedi, aradım aradım açmadı. ** ' ,'**En kahraman il hangisidir ? Cevap= Batman
- Espiri Sahibi = ** __**İbrahim / 14** ','**Tem yoluna muz düşmüş ne olmuş ==temmuz... Espiri Sahibi = ** __**Erdem / 13**__','**Adamın biri varmış 2.dönem düzelmtiş... Espiri Sahibi = ** __**^ Kaan / 16
-**__'];
+   message.channel.send('Espirimi istiyorsun Hemen Geliyor Bir Tane...').then(message => {
+   var espriler = ['**Ayda 500 dolar kazmak istermis o zaman aya Çık... Espiri Sahibi = ** __**Erkan - ByBaGo / 15**__', '**Rock yapmayan ki�iye ne denir? - Yaprock. ** ' , '**Sinemada on dakika ara dedi, arad�m arad�m a�mad�. ** ' ,'**En kahraman il hangisidir ? Cevap= Batman Espiri Sahibi = ** __**�brahim / 14** ','**Tem yoluna muz d��m�� ne olmu� ==temmuz... Espiri Sahibi = ** __**Erdem / 13**__','**Adam�n biri varm�� 2.d�nem d�zelmti�... Espiri Sahibi = ** __**^ Kaan / 16**__'];
       var espri = espriler[Math.floor(Math.random() * espriler.length)];
             message.edit(`${espri}`);
  });
