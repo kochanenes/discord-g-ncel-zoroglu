d8ae4d1d7e745518c4965d47707e83f8c9f99f2d
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-05-24 17:43:51 +0300

    Update hacked.js

--- komutlar/hacked.js
+++ komutlar/hacked.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
 
 Jimp.read(user.avatarURL, (err, image) => {
     image.resize(315, 310)
-    Jimp.read("http://kisi.deu.edu.tr//akcan.poyraz/1-580x386.JPG", (err, avatar) => {
+    Jimp.read("https://i.hizliresim.com/gPzGq3.jpg", (err, avatar) => {
         avatar.resize(315, 320)
         image.composite(avatar, 0, 0).write(`./img/snip/${client.user.id}-${user.id}.png`);
         setTimeout(function() {
