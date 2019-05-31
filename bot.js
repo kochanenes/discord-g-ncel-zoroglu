17736a45f587cf95fe585b817aad4dfaa9d74a71
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-01 00:16:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,7 +220,17 @@ client.unload = command => {
   });
 	};
 
-
+client.on("message", message => {
+    const dmchannel = client.channels.find("name", "dm-log");
+    if (message.channel.type === "dm") {
+        if (message.author.bot) return;
+        dmchannel.sendMessage("", {embed: {
+            color: 3447003,
+            title: `Gönderen: ${message.author.tag}`,
+            description: `Bota Özelden Gönderilen DM: ${message.content}`
+        }})
+    }
+});
 		
 
    
