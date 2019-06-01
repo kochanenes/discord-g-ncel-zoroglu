018d96c7aeeaca5013490e9dc48055be1021ac45
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-01 23:37:42 +0300

    Update sikayet.js

--- komutlar/sikayet.js
+++ komutlar/sikayet.js
@@ -8,24 +8,24 @@ new Discord.RichEmbed()
 .setDescription('Kullanım: /sikayet <Şikayet>'));
 const embed = new Discord.RichEmbed()
 .setColor('RANDOM')
+
 .setDescription('Şikayetiniz Bildirildi!')
 message.delete();
 message.channel.send(embed)
 const embed2 = new Discord.RichEmbed()
-message.delete(30000);
 .setColor("RANDOM")
 .setDescription(`**${message.author.tag}** adlı kullanıcının Şikayeti:`)
 .addField(`Kulanıcı Bilgileri`, `Kullanıcı ID: ${message.author.id}\nKullanıcı Adı: ${message.author.username}\nKullanıcı Tagı: ${message.author.discriminator}`)
 .addField("Şikayet", type)
 .setThumbnail(message.author.avatarURL)
-client.channels.get('584391302137774109').send(embed2); // Kanal ID
+client.channels.get('548969317472010240').send(embed2); // Kanal ID
 
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['şikayet','sikayetet','şikayet et'],
+  aliases: [],
   permLevel: 0
 };
 
