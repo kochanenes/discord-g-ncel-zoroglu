8bf7d2c0a03357b5fe976b74599b60d7a7faa7d9
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-01 19:28:49 +0300

    Update sikayet.js

--- komutlar/sikayet.js
+++ komutlar/sikayet.js
@@ -13,6 +13,7 @@ const embed = new Discord.RichEmbed()
 message.delete();
 message.channel.send(embed)
 const embed2 = new Discord.RichEmbed()
+message.delete(30000);
 .setColor("RANDOM")
 .setDescription(`**${message.author.tag}** adlı kullanıcının Şikayeti:`)
 .addField(`Kulanıcı Bilgileri`, `Kullanıcı ID: ${message.author.id}\nKullanıcı Adı: ${message.author.username}\nKullanıcı Tagı: ${message.author.discriminator}`)
