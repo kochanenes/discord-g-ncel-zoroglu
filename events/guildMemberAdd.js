9c0cce849956fd5bac0c6cf80bb3c0b2321cbb5e
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-03 19:43:06 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.sendMessage('**Selamlar,Değerli** __**' + username + '**__ **Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle <3 :wink: ** https://i.ibb.co/NSwhgf1/bella-hosgeldin.gif');
+    member.sendMessage('**Selamlar,Değerli** __**' + username + '**__ **Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar a bakmayı unutma,lazım olacağından eminim. :thinking: Keyifli Vakit Geçirmen Dileğiyle :hearts: :wink: ** https://i.ibb.co/NSwhgf1/bella-hosgeldin.gif');
 
 };
