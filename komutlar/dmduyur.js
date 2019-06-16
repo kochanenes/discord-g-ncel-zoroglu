59af70c6be31dd745b03a5be83524dff2f4dbd3e
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-16 20:48:55 +0300

    Create dmduyur.js

---
+++ komutlar/dmduyur.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+
+exports.run = (client, message, args) => {
+
+    if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0x2488E7)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('**Trex Duyuru Sistemi.**')
+    return message.author.sendEmbed(ozelmesajuyari); }
+
+  let mesaj = args.slice(0).join(' ');
+if (mesaj.length < 1) return message.channel.send('**Birşey Yazmalısınız**');
+
+  message.delete();
+
+  console.log(`Duyuru: "${message.author.username}#${message.author.discriminator}" "${mesaj}"`);
+
+      const mesajat = new Discord.RichEmbed()
+      .setColor('RANDOM')
+      .setDescription('' + mesaj + '')
+message.guild.members.filter(u =>
+u.sendEmbed(mesajat)
+)
+
+message.channel.send(`:white_check_mark: **Mesaj basariyla gonderildi.**`);
+
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['duyurla'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'dmduyuru',
+  description: 'İstediğiniz şeyi bota duyurtur.',
+  usage: 'duyuru [duyurmak istediğiniz şey]'
+};
