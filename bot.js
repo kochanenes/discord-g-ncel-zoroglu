f07bca467a2f10cbc8a70ce709522d1ac9c0c1f7
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-22 19:33:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -424,24 +424,6 @@ if (msg.content === 'SERGEN ABİ') {
 
 
 
-client.elevation = message => {
-  if(!message.guild) {
-	return; }
-  let permlvl = 0;
-  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
-  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
-  if (message.author.id === ayarlar.sahip) permlvl = 4;
-  return permlvl;
-};
 
-var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
-
-client.on('warn', e => {
-  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
-});
-
-client.on('error', e => {
-  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
-});
 
 client.login(process.env.BOT_TOKEN);
