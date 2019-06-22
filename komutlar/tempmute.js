1ee72b9cf91602f2615dbca6e63e16f9ff03940d
kochanenes <47575487+kochanenes@users.noreply.github.com>
2019-06-22 18:40:25 +0300

    Update tempmute.js

--- komutlar/tempmute.js
+++ komutlar/tempmute.js
@@ -33,11 +33,11 @@ module.exports.run = async (bot, message, args) => {
   if(!mutetime) return message.reply("**Lütfen Zaman Belirt...**\n**NOT: 1saat vereceksen 1h , 1dakika vereceksen 1m, 1gün vereceksen 1d  yazman yeterli 1 yerine İstediğin Bir Değer Yazabilirsin... **");
 
   await(tomute.addRole(muterole.id));
-  message.reply(`<@${tomute.id}> **Adlı Kullanıcıya Mute Verildi Verilen Zaman :**  ${ms(ms(mutetime))}`);
+  message.reply(`<@${tomute.id}> **Adlı Kullanıcıya Mute Verildi Verilen Zaman :** ``` ${ms(ms(mutetime))} ``` `);
 
   setTimeout(function(){
     tomute.removeRole(muterole.id);
-    message.channel.send(`<@${tomute.id}> has been unmuted!`);
+    message.channel.send(`<@${tomute.id}> **Mutedi Kaldırıldı...**`);
   }, ms(mutetime));
 
 
